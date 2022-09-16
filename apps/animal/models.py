0ef1761a40d715cdd3f88d1725b0f130f8a78c22
gabrielfmelo <gabrielfm2000@gmail.com>
2022-09-16 10:16:20 +0100

    inicializacao da app animal

--- apps/animal/models.py
+++ apps/animal/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Animal(models.Model):
-    nome = models.CharField(max_length=50)
-    especie = models.CharField(max_length=30)
-    idade = models.IntegerField()
