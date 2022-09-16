36bb32b4fa4684b3662180f6234e8df862c07af2
gabrielfmelo <gabrielfm2000@gmail.com>
2022-09-16 11:08:38 +0100

    commit 5

--- apps/animal/models.py
+++ apps/animal/models.py
@@ -5,3 +5,7 @@ class Animal(models.Model):
     nome = models.CharField(max_length=50)
     especie = models.CharField(max_length=30)
     idade = models.IntegerField()
+
+    def __str__(self):
+        # return self.nome
+        return f'{self.especie} : {self.nome}'
