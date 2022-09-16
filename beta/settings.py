a57a1e3a3248d9042ccfadecfd8f4e69a7dd95cf
gabrielfmelo <gabrielfm2000@gmail.com>
2022-09-16 11:09:49 +0100

    commit 1

--- beta/settings.py
+++ beta/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.animal.apps.PessoaConfig'
+    'apps.pessoa.apps.PessoaConfig'
     'apps.animal.apps.AnimalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
